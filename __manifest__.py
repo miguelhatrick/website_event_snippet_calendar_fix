3c2a19f7e4b4a4f65b84a9d543989c64527164f7
Miguel <miguelhatrick@gmail.com>
2021-03-19 18:56:11 -0300

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2021 Miguel Hatrick (Dacosys)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "website_event_snippet_calendar_fix",
+    "summary": """
+            Fixes the problem with timezones on non UTC events
+    """,
+    "version": "12.0.1.0.1",
+    "author": "Miguel Hatrick",
+    "license": "AGPL-3",
+    "maintainer": "Dacosys",
+    "category": "Extra Tools",
+    "website": "https://www.dacosys.com",
+    "depends": [
+        "website_event_snippet_calendar",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
---
+++ controllers/main.py
@@ -0,0 +1,44 @@
+# Copyright 2021 - Dacosys - Miguel Hatrick
+
+from datetime import date, timedelta, datetime
+
+import pytz
+from odoo.http import request, route
+from odoo.addons.website_event_snippet_calendar.controllers.main import EventCalendar
+
+
+class EventCalendarExtend(EventCalendar):
+    @route("/website_event_snippet_calendar/events_for_day", auth="public", type="json", website=True)
+    def events_for_day(self, day=None, limit=None):
+        """List events for a given day.
+
+        :param day string:
+            Date in a string. If ``None``, we'll search for upcoming events
+            from today up to specified limit.
+
+        :param limit int:
+            How many results to return.
+        """
+
+        _cd = date.today()
+        if day:
+            _cd = datetime.strptime(day, '%Y-%m-%d')
+
+        # Take to UTC
+        _cd = _cd.astimezone().astimezone(pytz.utc)
+
+        domain = [("date_end", "<=", (_cd + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S"))]
+        domain += [("date_begin", ">=", _cd.strftime("%Y-%m-%d %H:%M:%S"))]
+
+        return request.env["event.event"].search_read(
+            domain=domain,
+            limit=limit,
+            fields=[
+                "date_begin_pred_located",
+                "name",
+                "event_type_id",
+                "website_published",
+                "website_url",
+            ],
+        )
+
